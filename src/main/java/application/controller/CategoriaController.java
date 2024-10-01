320a067d9b13bb4efbd7dcadccc4d334833aa52f
Gabriel Garcia <117932713+gabrielhogarcia@users.noreply.github.com>
2024-10-01 13:41:01 -0300

    Create CategoriaController.java

---
+++ src/main/java/application/controller/CategoriaController.java
@@ -0,0 +1,29 @@
+package application.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import application.repository.CategoriaRepository;
+import application.model.Categoria;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+    @Autowired
+    CategoriaRepository categoriaRepo;
+
+    @GetMapping
+    public Iterable<Categoria> findAll() {
+        return categoriaRepo.findAll();
+    
+    }
+
+    @PostMapping
+    public Categoria insert(@RequestBody Categoria categoria) {
+        return categoriaRepo.save(categoria);
+    }
+}
